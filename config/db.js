9755d0328a6a71e4719b9a15b185a55c764582e2
jsk1010 <devilprotocol007@gmail.com>
2023-03-01 18:46:55 +0530

    mongo

--- config/db.js
+++ config/db.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set("strictQuery", false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        
+    console.log(process.env.MONGODB_URI.toString())
+        const conn = await mongoose.connect(process.env.MONGODB_URI.toString(), { useNewUrlParser: true, useUnifiedTopology: true });
 
         console.log(`Mongo DB connected ${conn.connection.host}`)
     } catch (error) {
